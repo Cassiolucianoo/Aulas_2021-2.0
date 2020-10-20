89b5a385928dbe60fda75c1efe3fd3ef0c1dedf6
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-10-19 22:02:19 -0300

    Nosso primeiro if
    - PRIMEIRO IF
    - COMENTARIOS

---
+++ Fundamentos/primeiroIf.js
@@ -0,0 +1,15 @@
+let nota = 8
+
+if( nota >= 7){
+    console.log('Aprovado '+nota)
+}
+   
+if(nota <= 6){
+    console.log('Reprovado '+nota)
+}
+    
+//console.log(nota+" * "+nota+" = "+( nota * nota ))
+
+//console.log(`${nota} * ${nota} = ${nota * nota}`)
+
+
